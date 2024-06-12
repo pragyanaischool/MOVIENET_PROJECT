2ed0f56e03827d6cf2cb42884227feb0feef3c3f
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2024-06-12 22:32:36 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -177,7 +177,7 @@ def main():
     st.title("Computer Vision Streamlit application")
     webrtc_streamer(key="demo", video_processor_factory=VideoTransformer) 
 
-    picture = st.camera_input("Take a picture", key ="secondcamera", disabled =True)
+    picture = st.camera_input("Take a picture", key ="secondcamera", disabled =False)
 
     if picture:
         st.image(picture)
